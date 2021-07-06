de9a76c1d05a642bd4bd1fd8acf7e1c1120794ed
oc <oskar.cejman@nxo.pl>
2021-07-06 15:02:42 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import {zmienna, zmienna2} from './message.js';
+zmienna("działam po bundlingu");
+zmienna2("działam heh xD")
---
+++ src/message.js
@@ -0,0 +1,2 @@
+export const zmienna = (item) => console.log(item)
+export const zmienna2 = (item) => document.querySelector('body').textContent = item;
